9e379b60b382132895c8376ac364299ee1015810
Yashraj Thelkar <96432126+yashraj2021@users.noreply.github.com>
2023-10-29 17:10:08 +0000

    Datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer JS version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    
+    +3) // readability is very important
+
+    console.log("Yashraj")
+  
+    let name = "Krsna"   // string datatype
+    let age = 16         // int datatype
+    let isLoggedIn = false // boolean datatype
+
+console.log(typeof "Krsna")
+
+// Interview que: null is an object
